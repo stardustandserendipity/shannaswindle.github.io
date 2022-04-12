0c1a7daee5a3f6c796490f39d91e6219a14246b7
Shanna Swindle <97823453+stardustandserendipity@users.noreply.github.com>
2022-04-11 21:57:56 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 
 if($_POST["message"]) {
 
-mail("your@email.address", "Here is the subject line",
+mail("sswin501@gmail.com", "Here is the subject line",
 
 $_POST["insert your message here"]. "From: an@email.address");
 
